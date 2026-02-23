c89f026aa8651ba8a57df0fff7016f413277234c
vai <138060923+vai-tv@users.noreply.github.com>
2026-02-23 13:19:16 +0000

    Refactor Scale class constructor to improve parameter naming and enhance string representation

--- generator/scale.py
+++ generator/scale.py
@@ -5,19 +5,20 @@ SCALES: dict[str, list[int]] = json.load(open("scales.json"))
 
 class Scale:
 
-    def __init__(self, root: Note, scale_map: list[int]):
+    def __init__(self, root: Note, name: str, map: list[int]):
 
-        assert max(scale_map) < 12, "Scale intervals must be less than 12 semitones"
+        assert max(map) < 12, "Scale intervals must be less than 12 semitones"
 
         self.root = root
-        self.scale_map = scale_map
+        self.name = name
+        self.map = map
 
     def __getitem__(self, degree: int) -> int:
         """Returns the interval in semitones for the given degree of the scale."""
 
         index = degree - 1  # Convert to 0-based index
-        octave, degree_in_octave = divmod(index, len(self.scale_map))
-        return self.scale_map[degree_in_octave] + 12 * octave + self.root.pitch
+        octave, degree_in_octave = divmod(index, len(self.map))
+        return self.map[degree_in_octave] + 12 * octave + self.root.pitch
     
     def __str__(self) -> str:
-        return f"Scale(root={self.root}, scale_map={self.scale_map})"
+        return f"{self.root.name[:-1].capitalize()} {self.name.replace('_', ' ').capitalize()} Scale"
