ce933485fad1432ac6d5ff164d09081e2c5a7ccc
vai <138060923+vai-tv@users.noreply.github.com>
2026-02-23 10:57:31 +0000

    Add random modifications

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from unittest import case
 
 from generator.mod import Mod
 from generator.chord import Chord
@@ -22,6 +23,30 @@ def random_scale() -> Scale:
     scale_name = random.choice(list(SCALES.keys()))
     return Scale(root=Note(pitch=random.randint(40, 51)), scale_map=SCALES[scale_name])
 
+def random_mod(scale, chromatic_chance: float=0.5) -> Mod:
+    """Generates a random modifier for a chord."""
+    mod = {}
+
+    # If not chromatic, keep looping until the resulting interval is diatonic
+    chromatic = random.random() < chromatic_chance
+
+    while True:
+        instruction = random.choice(["add", "no", "#", "b", "sus"])
+        match instruction:
+            case "add":
+                interval = random.choice([6, 7, 9, 11, 13])
+            case "sus":
+                interval = random.choice([2, 4, 6])
+            case _:
+                interval = random.choice([1, 3, 5])
+
+        c = Chord(scale, 1, modifiers=Mod({instruction: interval}))
+        if c.is_diatonic() or chromatic:
+            mod[instruction] = interval
+            break
+    
+    return Mod(mod) if mod else None
+
 def progression(scale, n: int=4) -> list[Chord]:
     """Generates a progression of chords."""
     
@@ -34,7 +59,13 @@ def progression(scale, n: int=4) -> list[Chord]:
             next_degrees = list(progression_map[degree])
         next_degree = random.choice(next_degrees)
         used_degrees.add(next_degree)
-        chord = Chord(scale, next_degree)
+
+        # Add a random modifier to the chord
+        if random.random() < 1 and Chord(scale, next_degree).type != "unknown":  # 30% chance to add a modifier
+            mod = random_mod(scale)
+            print(mod)
+
+        chord = Chord(scale, next_degree, modifiers=mod if 'mod' in locals() else None)
         progression.append(chord)
 
     # Add a cadence
